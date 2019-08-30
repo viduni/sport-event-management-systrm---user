9b3264659689d118f6dfa7e637e374628632ea2d
Viduni Randima <mahaduravidur@gmail.com>
2019-08-30 18:09:04 +0530

    Add all files

---
+++ inuser_badmintoncourt.php
@@ -0,0 +1,15 @@
+<?php 
+	include 'dbh.php';
+	$eventname=$_POST['eventname'];
+	$dateOF=$_POST['dateOF'];
+	$timePeriod=$_POST['timePeriod'];
+	$userName=$_POST['userName'];
+	$userPost=$_POST['userPost'];
+	$email=$_POST['email'];
+	
+	
+	$sql="INSERT INTO user_badmintoncourt(eventname,dateOF,timePeriod,userName,userPost,email) VALUES ('$eventname','$dateOF','$timePeriod','$userName','$userPost','$email')";
+	$result=$conn->query($sql);
+	
+	header('Location:book_badmintoncourt.php');
+?>
